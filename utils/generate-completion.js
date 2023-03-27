dc1a2a3de026cbe4a32462b444c11e2cf41dfe1a
kennnnny <kennnnny_15@hotmail.com>
2023-03-28 07:15:37 +0800

    Update parameter for gpt-4

--- utils/generate-completion.js
+++ utils/generate-completion.js
@@ -31,7 +31,8 @@ const generateCompletion = async ({
   prompt,
 }) => {
   if (config.APP_ENV !== 'production') return new Completion({ text: MOCK_TEXT_OK });
-  if (config.OPENAI_COMPLETION_MODEL.includes(MODEL_GPT_3_5_TURBO)) {
+  if (config.OPENAI_COMPLETION_MODEL.includes(MODEL_GPT_3_5_TURBO)
+  ||config.OPENAI_COMPLETION_MODEL.includes(MODEL_GPT_4)) {
     const { data } = await createChatCompletion({ messages: prompt.messages });
     const [choice] = data.choices;
     return new Completion({
